67d2935701d1bd1b600f6b6dcda4824b67dd666e
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-03 19:14:46 -0500

    Creacion de Interfaces y Servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { TablaComponent } from './shared/tabla/tabla.component';
+import { RecursosService } from './servicios/recursos.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Foto } from './interfaz/foto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContactoComponent, TablaComponent],
+  imports: [ContactoComponent, TablaComponent,HttpClientModule],
+  providers: [RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyectoAngular';
+  fotos: Foto[] = [];
+
+  constructor(private recursoService: RecursosService) {
+    this.recursoService.obtenerDatos().subscribe(respuesta =>{
+      this.fotos = respuesta as Array<Foto>;
+    })
+  }
 }
 
